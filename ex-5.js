09a3064910b0c05ccfc429b6ed73959bca8117b4
Titima Kasemsritanawat <titima.kp@gmail.com>
2024-04-04 00:09:58 +0700

    Update answer for ex-5.js

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,5 @@
 // Exercise #5
-let getJohnProfile = () => {
+let getJohnProfile = async () => {
   return new Promise(function (_, reject) {
     setTimeout(
       () =>
@@ -12,9 +12,13 @@ let getJohnProfile = () => {
   });
 };
 // Start coding here
-try{
-  let profile = await getJohnProfile();
-  console.log(profile);
-}catch(e){
-  console.log(e);
+async function asynchronousFunction(){
+  try{
+    let profile = await getJohnProfile();
+    console.log(profile);
+  }catch(e){
+    console.log(e);
+  }
 }
+
+asynchronousFunction();
